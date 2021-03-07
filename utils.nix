cb3898270d2da677d85cf5ddf5acc7cf16356ba4
Asad Saeeduddin <masaeedu@gmail.com>
2021-03-06 22:51:50 -0500

    Decompose a bit more

--- utils.nix
+++ utils.nix
@@ -33,10 +33,10 @@ rec {
 
   set =
   rec {
-    # k -> { [k]: v, ...r } -> v /\ r
+    # k -> { [k] = v; ...r } -> v /\ r
     uncons = k: r: { fst = r."${k}"; snd = builtins.removeAttrs r [k]; };
 
-    # k -> v /\ r -> { [k]: v, ...r }
+    # k -> v /\ r -> { [k] = v; ...r }
     cons = k: { fst, snd }: snd // { "${k}" = fst; };
   };
 
@@ -44,8 +44,11 @@ rec {
   with p;
   rec {
 
-    # k -> p a b -> p { [k] = a; ...r } { [k] = b; ...r }
-    key = k: pab: dimap (set.uncons k) (set.cons k) (first pab);
+    # k -> Iso { [k] = a; ...r } { [k] = b; ...r } (a /\ r) (b /\ r)
+    unconsed = k: dimap (set.uncons k) (set.cons k);
+
+    # k -> Lens { [k] = a; ...r } { [k] = b; ...r } a b
+    key = k: fn.pipe [(unconsed k) first];
 
     # given a list of keys, produces an optic that focuses on the relevant
     # part of an appropriately keyed nested attrset
